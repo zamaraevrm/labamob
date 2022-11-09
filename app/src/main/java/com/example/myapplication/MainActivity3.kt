1be3024fe9327af7119724e19d59490cc282e0c3
zamaraevrm <zamaraevrm@mer.ci.nsu.ru>
2022-11-09 14:22:54 +0700

    jkhjkfd

--- app/src/main/java/com/example/myapplication/MainActivity3.kt
+++ app/src/main/java/com/example/myapplication/MainActivity3.kt
@@ -2,10 +2,21 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
 
 class MainActivity3 : AppCompatActivity() {
+
+    lateinit var db: contactDataBase
+    lateinit var contactDao: contactDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+        db = Room.databaseBuilder(
+            applicationContext,
+            contactDataBase::class.java, "contactdb"
+        ).allowMainThreadQueries().build()
+        contactDao = db.contactDao()
     }
 }
