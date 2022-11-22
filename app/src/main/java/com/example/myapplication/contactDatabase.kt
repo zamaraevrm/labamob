a687bb534e8457852189fd3cff8e89a754e827d9
bajarka bajarka <zamaraevrm@mer.ci.nsu.ru>
2022-11-22 22:40:32 +0700

    fgdfsdf

--- app/src/main/java/com/example/myapplication/contactDatabase.kt
+++ app/src/main/java/com/example/myapplication/contactDatabase.kt
@@ -4,10 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 
 @Database(entities = [ContactEntity::class], version = 1)
-@TypeConverters(Converters::class)
 abstract class ContactDatabase: RoomDatabase() {
     abstract fun contactDao(): ContactDao
 
