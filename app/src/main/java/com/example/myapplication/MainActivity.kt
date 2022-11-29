7fef83d21ac84dd3fa9b1eeac7ef8a09ce23a395
bajarka bajarka <zamaraevrm@mer.ci.nsu.ru>
2022-11-29 19:16:37 +0700

    add search

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,16 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     private val contactsList = mutableListOf<ContactEntity>()
+    //private lateinit var nonChagedContactList: List<ContactEntity>
+
     private lateinit var adapter: RecyclerAdapter
 
     companion object {
@@ -29,22 +33,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonAddContact = findViewById<Button>(R.id.buttonAddContact)
-        buttonAddContact.setOnClickListener{
-           val intent = Intent(this, UpdateCreateContactActivity::class.java)
-           startActivity(intent)
-        }
-
-        val editTextSearch = findViewById<EditText>(R.id.editTextTextPersonName)
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        lifecycleScope.launch(Dispatchers.IO){
-            contactsList.addAll(contactDatabase.getAllContacts)
-        }
-
         adapter = RecyclerAdapter(contactsList) {
             val intent = Intent(this, InfoAboutContactActivity::class.java)
             intent.putExtra(EXTRA_KEY, contactsList[it].id)
@@ -54,6 +42,40 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
+
+        val buttonAddContact = findViewById<Button>(R.id.buttonAddContact)
+        buttonAddContact.setOnClickListener{
+           val intent = Intent(this, UpdateCreateContactActivity::class.java)
+           startActivity(intent)
+        }
+
+        val editTextSearch = findViewById<EditText>(R.id.editTextTextPersonName)
+        editTextSearch.doOnTextChanged { text, start, before, count ->
+
+            lifecycleScope.launch(Dispatchers.IO) {
+                val list = contactDatabase.getAllContacts.filter { (it.firstname.indexOf(text.toString()) != -1) ||
+                                                        (it.surname?.indexOf(text.toString()) != -1)}
+                contactsList.clear()
+                contactsList.addAll(list)
+                withContext(Dispatchers.Main){
+                    recyclerView.adapter?.notifyDataSetChanged()
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        lifecycleScope.launch(Dispatchers.IO){
+            contactsList.addAll(contactDatabase.getAllContacts)
+            withContext(Dispatchers.Main){
+                val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+                recyclerView.adapter?.notifyDataSetChanged()
+            }
+        }
+
+
     }
 
     override fun onStop() {
